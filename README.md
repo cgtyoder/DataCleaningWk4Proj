e435b2315246c43574e61a5df6a9741f0afa1adc
Conrad G T Yoder <cgtyoder@alum.mit.edu>
2017-12-18 11:21:11 -0500

    New submission

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # DataCleaningWk4Proj
 
-There are 3 files in this repo:
+There are 4 files in this repo:
 
 * README.MD - this file
 * CodeBook.md - the Code Book for the data set, summary choices
 * run_analysis.R - R function "run_analysis()" which is the cookbook to get from the raw assignment data to the tidy data set. **The current working directory for this script must contain the folder "UCI HAR Dataset" as unzipped from the downloaded data set "UCI HAR Dataset.zip"**
+* tidyaverages.txt - output of run_analysis.R script
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,6 @@
 # R script to convert raw to tidy data in Data Cleaning class, Week 4 Project
+library(reshape2)
+
 
 run_analysis <- function()
 {
@@ -83,80 +85,8 @@ run_analysis <- function()
     
     # create new data frame with the ave of each var for each activity 
     # and each subject
-    newaccel <- cbind(
-        xtabs(TimeBodyAccelMeanX ~ SubjectID + Activity, data = aggregate(TimeBodyAccelMeanX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelMeanY ~ SubjectID + Activity, data = aggregate(TimeBodyAccelMeanY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelMeanZ ~ SubjectID + Activity, data = aggregate(TimeBodyAccelMeanZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelSDX ~ SubjectID + Activity, data = aggregate(TimeBodyAccelSDX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelSDY ~ SubjectID + Activity, data = aggregate(TimeBodyAccelSDY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelSDZ ~ SubjectID + Activity, data = aggregate(TimeBodyAccelSDZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeGravityAccelMeanX ~ SubjectID + Activity, data = aggregate(TimeGravityAccelMeanX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeGravityAccelMeanY ~ SubjectID + Activity, data = aggregate(TimeGravityAccelMeanY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeGravityAccelMeanZ ~ SubjectID + Activity, data = aggregate(TimeGravityAccelMeanZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeGravityAccelSDX ~ SubjectID + Activity, data = aggregate(TimeGravityAccelSDX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeGravityAccelSDY ~ SubjectID + Activity, data = aggregate(TimeGravityAccelSDY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeGravityAccelSDZ ~ SubjectID + Activity, data = aggregate(TimeGravityAccelSDZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkMeanX ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkMeanX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkMeanY ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkMeanY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkMeanZ ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkMeanZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkSDX ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkSDX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkSDY ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkSDY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkSDZ ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkSDZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroMeanX ~ SubjectID + Activity, data = aggregate(TimeBodyGyroMeanX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroMeanY ~ SubjectID + Activity, data = aggregate(TimeBodyGyroMeanY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroMeanZ ~ SubjectID + Activity, data = aggregate(TimeBodyGyroMeanZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroSDX ~ SubjectID + Activity, data = aggregate(TimeBodyGyroSDX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroSDY ~ SubjectID + Activity, data = aggregate(TimeBodyGyroSDY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroSDZ ~ SubjectID + Activity, data = aggregate(TimeBodyGyroSDZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkMeanX ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkMeanX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkMeanY ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkMeanY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkMeanZ ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkMeanZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkSDX ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkSDX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkSDY ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkSDY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkSDZ ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkSDZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkMeanX ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkMeanX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkMeanY ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkMeanY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkMeanZ ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkMeanZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkSDX ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkSDX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkSDY ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkSDY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkSDZ ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkSDZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelMagMean ~ SubjectID + Activity, data = aggregate(TimeBodyAccelMagMean ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelMagSD ~ SubjectID + Activity, data = aggregate(TimeBodyAccelMagSD ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkMagMean ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkMagMean ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkMagSD ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkMagSD ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkMagMean ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkMagMean ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyAccelJerkMagSD ~ SubjectID + Activity, data = aggregate(TimeBodyAccelJerkMagSD ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroMagMean ~ SubjectID + Activity, data = aggregate(TimeBodyGyroMagMean ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroMagSD ~ SubjectID + Activity, data = aggregate(TimeBodyGyroMagSD ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkMagMean ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkMagMean ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(TimeBodyGyroJerkMagSD ~ SubjectID + Activity, data = aggregate(TimeBodyGyroJerkMagSD ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelMeanX ~ SubjectID + Activity, data = aggregate(FFTBodyAccelMeanX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelMeanY ~ SubjectID + Activity, data = aggregate(FFTBodyAccelMeanY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelMeanZ ~ SubjectID + Activity, data = aggregate(FFTBodyAccelMeanZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelSDX ~ SubjectID + Activity, data = aggregate(FFTBodyAccelSDX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelSDY ~ SubjectID + Activity, data = aggregate(FFTBodyAccelSDY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelSDZ ~ SubjectID + Activity, data = aggregate(FFTBodyAccelSDZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelJerkMeanX ~ SubjectID + Activity, data = aggregate(FFTBodyAccelJerkMeanX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelJerkMeanY ~ SubjectID + Activity, data = aggregate(FFTBodyAccelJerkMeanY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelJerkMeanZ ~ SubjectID + Activity, data = aggregate(FFTBodyAccelJerkMeanZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelJerkSDX ~ SubjectID + Activity, data = aggregate(FFTBodyAccelJerkSDX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelJerkSDY ~ SubjectID + Activity, data = aggregate(FFTBodyAccelJerkSDY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelJerkSDZ ~ SubjectID + Activity, data = aggregate(FFTBodyAccelJerkSDZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroMeanX ~ SubjectID + Activity, data = aggregate(FFTBodyGyroMeanX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroMeanY ~ SubjectID + Activity, data = aggregate(FFTBodyGyroMeanY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroMeanZ ~ SubjectID + Activity, data = aggregate(FFTBodyGyroMeanZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroSDX ~ SubjectID + Activity, data = aggregate(FFTBodyGyroSDX ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroSDY ~ SubjectID + Activity, data = aggregate(FFTBodyGyroSDY ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroSDZ ~ SubjectID + Activity, data = aggregate(FFTBodyGyroSDZ ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelMagMean ~ SubjectID + Activity, data = aggregate(FFTBodyAccelMagMean ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelMagSD ~ SubjectID + Activity, data = aggregate(FFTBodyAccelMagSD ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelJerkMagMean ~ SubjectID + Activity, data = aggregate(FFTBodyAccelJerkMagMean ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyAccelJerkMagSD ~ SubjectID + Activity, data = aggregate(FFTBodyAccelJerkMagSD ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroMagMean ~ SubjectID + Activity, data = aggregate(FFTBodyGyroMagMean ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroMagSD ~ SubjectID + Activity, data = aggregate(FFTBodyGyroMagSD ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroJerkMagMean ~ SubjectID + Activity, data = aggregate(FFTBodyGyroJerkMagMean ~ SubjectID + Activity, tidyset, mean)),
-        xtabs(FFTBodyGyroJerkMagSD ~ SubjectID + Activity, data = aggregate(FFTBodyGyroJerkMagSD ~ SubjectID + Activity, tidyset, mean))
-    )
+    meltTidy <- melt(tidyset, id = c("SubjectID", "Activity"))
+    newaccel <- dcast(meltTidy, SubjectID + Activity ~ variable, mean)
     
     # save new data frame to txt file
     write.table(newaccel, file = "tidyaverages.txt", row.names = FALSE)
